31dd9e2838778c312d17341ec92f63668637bd6d
Krishaad <xxk150@student.bham.ac.uk>
2024-07-19 13:54:21 +0100

    I have commented out my half of the files

--- WGT-BankingApplication/StandingOrder.cs
+++ WGT-BankingApplication/StandingOrder.cs
@@ -1,12 +1,15 @@
 ﻿namespace WGT_BankingApplication;
 
+// The StandingOrder class inherits from the PersonalAccount class
 class StandingOrder : PersonalAccount
 {
-    private string _payee;
-    private decimal _amount;
-    private DateTime _date;
-    private string _reference;
+    private string _payee;//the payee of the standing order
+    private decimal _amount;//amount of standing order
+    private DateTime _date;//date of standing order
+    private string _reference;//reference for standing order
 
+
+    //constructor for standing order class
     public StandingOrder(Customer Customer, decimal InitialDeposit, string Payee, decimal Amount, DateTime Date, string Reference) : base(Customer, InitialDeposit)
     {
         _payee = Payee;
@@ -15,5 +18,6 @@ class StandingOrder : PersonalAccount
         _reference = Reference;
     }
 
+    //prop to get and set the reference for the stading order
     public string Reference { get => _reference; set => _reference = value; }  // Added to keep the code working
 }
