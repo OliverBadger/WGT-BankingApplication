93d836a03649050b6631a4b17e21cc4b27578562
OB <oliverbadger@aol.com>
2024-07-19 12:22:13 +0100

    All comments added for my section

--- WGT-BankingApplication/Account.cs
+++ WGT-BankingApplication/Account.cs
@@ -2,30 +2,37 @@
 
 abstract class Account : Customer
 {
-    private string _accountNumber;
-    private decimal _balance = 0.00m;
+     
+    private string _accountNumber; // Stores the account number
+    private decimal _balance = 0.00m; // Stores the account balance, initialised to 0.00
 
+    // Property for account number to be read-only
     public string AccountNumber { get => _accountNumber; }
+    // Property to get and set the balance, balance can only be set within the class or derived classes
     public virtual decimal Balance { get => _balance; protected set => _balance = value; }
 
+    // Constructor to initialise an account with basic details and generate a unique account number
     public Account(int ID, string FirstName, string Surname, string Password, string CustomerNumber) : base(ID, FirstName, Surname, Password, CustomerNumber)
     {
         _accountNumber = GenerateRandomAccountNumber();
     }
 
+    // Abstract methods to be implemented by derived classes for account operations
     public abstract void OpenAccount();
     public abstract void CloseAccount();
     public abstract void Deposit(decimal amount);
     public abstract void Withdraw(decimal amount);
 
+    // Method to generate a random 8-digit number
     private string GenerateRandomAccountNumber()  // Rough implementation of generating a unique account number for each account
     {
         string randomNumber = "";
         Random rand = new Random();
+        // Append a random digit to the account number
         for (int i = 0; i < 8; i++)
         {
             randomNumber += rand.Next(0, 9);
         }
-        return randomNumber;
+        return randomNumber; // Return the generated account number
     }
 }
--- WGT-BankingApplication/DirectDebit.cs
+++ WGT-BankingApplication/DirectDebit.cs
@@ -2,18 +2,21 @@
 
 class DirectDebit : PersonalAccount
 {
-	private string _payee;
-	private decimal _amount;
-	private DateTime _date;
-	private string _reference;
+    private string _payee;         // The entity receiving the payment
+    private decimal _amount;       // The amount to be debited
+    private DateTime _date;        // The date of the transaction
+    private string _reference;     // Reference for the transaction
 
-	public DirectDebit(Customer Customer, decimal InitialDeposit, string Payee, decimal Amount, DateTime Date, string Reference) : base(Customer, InitialDeposit)
-	{
-		_payee = Payee;
-		_amount = Amount;
-		_date = Date;
-		_reference = Reference;
-	}
+    // Constructor to initialize DirectDebit with given details
+    public DirectDebit(Customer customer, decimal initialDeposit, string payee, decimal amount, DateTime date, string reference)
+        : base(customer, initialDeposit)
+    {
+        _payee = payee;              // Set the payee
+        _amount = amount;           // Set the amount
+        _date = date;               // Set the transaction date
+        _reference = reference;     // Set the transaction reference
+    }
 
+    // Gets and Sets transaction reference
     public string Reference { get => _reference; set => _reference = value; }  // Added to keep the code working
 }
